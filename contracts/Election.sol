24ab12113f39abf16e0eda2f23e0497bac62ce03
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2020-02-08 21:03:41 +0600

    Modified Election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,12 +2,26 @@ pragma solidity >=0.4.21 <0.7.0;
 
 // Contract definition
 contract Election {
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
     // Store candidate
-    // Read candidate
-    string public candidate;
+    // Fetch candidate
+    mapping(uint => Candidate) public candidates;
+    // Store candidates count
+    uint public candidatesCount;
     // Constructor
 
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
