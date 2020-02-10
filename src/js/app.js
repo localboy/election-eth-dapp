4de9403543951885fd2461007cf2f8bfd30dd4ca
Mohammad Jakir Hossain <mjakir.ddd@gmail.com>
2020-02-10 23:50:37 +0600

    Added fronted functionality to cast a vote

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 App = {
   web3Provider: null,
   contracts: {},
-  accounts: '0x0',
+  account: '0x0',
 
   init: function() {
     return App.initWeb3();
@@ -38,10 +38,9 @@ App = {
 
     loader.show();
     content.hide();
-
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
-      if (err === null) {
+      if (account !== null) {
         App.account = account;
         $("#accountAddress").html("Your Account: " + account);
       }
@@ -55,6 +54,9 @@ App = {
       var candidatesResults = $("#candidatesResults");
       candidatesResults.empty();
 
+      var candidatesSelect = $('#candidatesSelect');
+      candidatesSelect.empty();
+
       for (var i = 1; i <= candidatesCount; i++) {
         electionInstance.candidates(i).then(function(candidate) {
           var id = candidate[0];
@@ -64,15 +66,37 @@ App = {
           // Render candidate Result
           var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           candidatesResults.append(candidateTemplate);
+
+          // Render candidate ballot option
+          var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+          candidatesSelect.append(candidateOption);
         });
       }
-
+      return electionInstance.voters(App.account);
+    }).then(function(hasVoted){
+      // Do not allow a user to vote
+      if(hasVoted) {
+        $('form').hide();
+      }
       loader.hide();
       content.show();
     }).catch(function(error) {
       console.warn(error);
     });
   },
+
+  castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
+    App.contracts.Election.deployed().then(function(instance) {
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
 };
 
 $(function() {
